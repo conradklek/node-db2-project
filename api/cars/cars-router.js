9e3b4a81543a2ebd3d2d6b5d67d6e4c16b49de15
GooieJS <conradklek@gmail.com>
2022-04-19 22:42:32 -0500

    push

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,34 @@
-// DO YOUR MAGIC
+const express = require("express")
+
+const Car = require('./cars-model')
+
+const {
+    checkCarId,
+    checkVinNumberValid,
+    checkVinNumberUnique,
+    checkCarPayload
+} = require('./cars-middleware')
+
+const router = express.Router()
+
+router.get('/', async (req, res, next) => {
+    try {
+        const cars = await Car.getAll()
+        res.status(200).json(cars)
+    } catch (err) {
+        next(err)
+    }
+})
+router.get('/:id', checkCarId, async (req, res) => {
+    res.json(req.car)
+})
+router.post('/', checkCarPayload, checkVinNumberValid, checkVinNumberUnique, async (req, res) => {
+  try {
+    const car = await Car.create(req.body)
+    res.status(201).json(car)
+  } catch (err) {
+    res.status(500).json({ message: 'error creating car' })
+  }
+})
+
+module.export = router
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,30 @@
 // STRETCH
+const cars = [
+    {
+        vin: 'WBAWC73578E067076',
+        make: 'Honda',
+        model: 'Civic',
+        mileage: '10000',
+        title: 'clean',
+        transmission: 'manual',
+    },
+    {
+        vin: '1ZVFT80N475211367',
+        make: 'Honda',
+        model: 'Accord',
+        mileage: '20000',
+        title: 'clean',
+        transmission: 'manual',
+    },
+    {
+        vin: '5N1AN0NW9BC524974',
+        make: 'Honda',
+        model: 'CR-V',
+        mileage: '30000',
+    }
+]
+
+exports.seed = async (knex) => {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+};
